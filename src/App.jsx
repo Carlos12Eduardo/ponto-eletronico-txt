cd4f5e9b27185b3ae654b69f3ce89b5063f8b187
Carlos Silva <carlos110eduardo@gmail.com>
2026-04-22 23:33:13 -0300

    adicionado o metodo de ler arquivo txt

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import './App.css'
 
 function App() {
-    const leitorArquivo = (file) => {
-        const [conteudo, setConteudo] = useState(null)
+    const refInputFile = useRef();
+    const [conteudo, setConteudo] = useState("")
+    
+    const handleFileSubmit = (refInputFile) => {
+
+        const file = refInputFile.files[0];
+        console.log("dados do arquivo", file.type);
+        
+        if (file && file.type === "text/plan") {
+            const reader = new FileReader();
+            
+            reader.onload = (e) => {
+                console.log(e.target.result);
+                setConteudo(e.target.result);
+            }
+            reader.readAsText(file);
+        }
+        else {
+            alert("Nenhum arquivo .txt selecionado");
+        }
     }
     return (
         <>
             <label htmlFor="file">Importar arquivo txt</label>
-            <input type="file" name="file" id="file" />
+            <input type="file" name="file" id="file" ref={refInputFile}/>
+            <input type="button" value="Enviar" onClick={() => handleFileSubmit(refInputFile.current)} />
+            <p>{conteudo}</p>
         </>
     )
 }

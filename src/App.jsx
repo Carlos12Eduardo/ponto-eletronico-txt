03ecad6fe1b83e8fc4eb2338db8652c90da35a4b
Carlos Silva <carlos110eduardo@gmail.com>
2026-06-01 19:16:47 -0300

    aplicado as cores no app.jsx e alterando a imagem do menu hamburguer

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,13 @@ function App() {
     const [funcionarioSelecionado, setFuncionarioSelecionado] = useState("");
 
     const handleFileSubmit = async (inputFile) => {
-        try{
+        try {
             const response = await converterArquivoPontoTxtParaJson(inputFile);
-            if(response){
+            if (response) {
                 setConteudo(response);
                 alert('Arquivo enviado com sucesso!');
             }
-        } catch(error){
+        } catch (error) {
             alert(`ocorreu um erro ao tentar ler o arquivo txt. ${error}`);
         }
     }
@@ -26,21 +26,27 @@ function App() {
         <div className="container">
             <header>
                 <a href="#">
-                    <img src="./src/assets/images/more.png" width="20px" height="20px"/>
+                    <img src="./src/assets/images/more.png" width="20px" height="20px" />
                 </a>
                 <h2>Ponto Eletrônico</h2>
                 <a href="#"> alterar tema</a>
             </header>
             <aside>
-                <label htmlFor="file">Importar arquivo txt</label>
-                <input type="file" name="file" id="file" onChange={(e) => setInputFile(e.target.files[0])} />
-                <input type="button" value="Enviar" onClick={() => handleFileSubmit(inputFile)} />
-                <ListaFuncionarios
-                    funcionarios={conteudo.funcionarios}
-                    funcionarioSelecionado={funcionarioSelecionado}
-                    setFuncionarioSelecionado={setFuncionarioSelecionado}
-                />
-                <a href="#" onClick={() => alert("Ainda em desenvolvimento")}>Exportar CSV</a>
+                <div className="input-field">
+                    <label htmlFor="file">Importar arquivo txt</label>
+                    <input type="file" name="file" id="file" onChange={(e) => setInputFile(e.target.files[0])} />
+                    <input type="button" value="Enviar" onClick={() => handleFileSubmit(inputFile)} />
+                </div>
+                <div className="input-field">
+                    <ListaFuncionarios
+                        funcionarios={conteudo.funcionarios}
+                        funcionarioSelecionado={funcionarioSelecionado}
+                        setFuncionarioSelecionado={setFuncionarioSelecionado}
+                    />
+                </div>
+                <div className="input-field">
+                    <a href="#" onClick={() => alert("Ainda em desenvolvimento")}>Exportar CSV</a>
+                </div>
             </aside>
             <main>
                 <h3>Tabela de pontos registrados</h3>
